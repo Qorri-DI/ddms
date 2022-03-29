025860d4d5207bb67fc1216162019d35af7daa15
fahrur huzain <fhuzain96@gmail.com>
2022-03-29 18:11:21 +0700

    fahrur

--- src/main/java/com/alpha/ddms/services/DealerService.java
+++ src/main/java/com/alpha/ddms/services/DealerService.java
@@ -1,6 +1,7 @@
 package com.alpha.ddms.services;
 
 import com.alpha.ddms.repositories.DealerRepository;
+import com.alpha.ddms.repositories.SalesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,9 @@ import javax.transaction.Transactional;
 public class DealerService {
     @Autowired
     DealerRepository dealerRepository;
+
+    @Autowired
+    SalesRepository salesRepository;
 
 
 }
