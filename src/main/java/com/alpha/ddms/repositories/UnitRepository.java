c7303eae21d0f2ee382c777734835260a14a0851
Qorri Dwi Istajib <qorri24j@gmail.com>
2022-03-29 17:02:22 +0700

    Qorri

--- src/main/java/com/alpha/ddms/repositories/UnitRepository.java
+++ src/main/java/com/alpha/ddms/repositories/UnitRepository.java
@@ -1,7 +1,9 @@
 package com.alpha.ddms.repositories;
 
-import com.alpha.ddms.domains.UnitModel;
+import com.alpha.ddms.domains.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.*;
 
+@Repository
 public interface UnitRepository extends JpaRepository<UnitModel, String> {
 }
