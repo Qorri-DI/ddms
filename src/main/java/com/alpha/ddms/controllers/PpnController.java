609dc53c61151d494e0f6c9096b4929e248e219f
Arham Aulia Nugraha <azkakhowarizmi@gmail.com>
2022-03-29 17:14:12 +0700

    add PpnController

--- src/main/java/com/alpha/ddms/controllers/PpnController.java
+++ src/main/java/com/alpha/ddms/controllers/PpnController.java
@@ -1,7 +1,10 @@
 package com.alpha.ddms.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(name = "ddms/v1/cmd/master/ppn")
 public class PpnController {
+
 }
