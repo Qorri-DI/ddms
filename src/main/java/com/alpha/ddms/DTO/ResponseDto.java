ed1b5b2c31989fb872e468658147453dc9bc8c52
Anton Wijaya <antonhuang91@gmail.com>
2022-03-31 09:26:32 +0700

    ganti ResponseDto dgn lombok annotation

--- src/main/java/com/alpha/ddms/DTO/ResponseDto.java
+++ src/main/java/com/alpha/ddms/DTO/ResponseDto.java
@@ -1,12 +1,8 @@
 package com.alpha.ddms.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class ResponseDto<T> {
     private String status;
     private Integer code;
