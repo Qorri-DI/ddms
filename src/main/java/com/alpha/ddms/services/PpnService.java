bab85b95fd50cba343244ce42b77b71e751e80d4
Anton Wijaya <antonhuang91@gmail.com>
2022-03-31 10:17:33 +0700

    merge dengan branch Arham

--- src/main/java/com/alpha/ddms/services/PpnService.java
+++ src/main/java/com/alpha/ddms/services/PpnService.java
@@ -1,14 +1,34 @@
 package com.alpha.ddms.services;
 
+import com.alpha.ddms.domains.PpnModel;
 import com.alpha.ddms.repositories.PpnRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 @Transactional
 public class PpnService {
+
     @Autowired
     PpnRepository ppnRepository;
+
+    public Optional<PpnModel>findPpnId(String id){
+        Optional<PpnModel>getPpnId = ppnRepository.findById(id);
+
+        return getPpnId;
+    }
+
+    public PpnModel savePpn(PpnModel ppnModel){
+        PpnModel save = ppnRepository.save(ppnModel);
+
+        return save;
+    }
+    public PpnModel updatePpn(PpnModel ppnModel){
+        PpnModel save = ppnModel;
+
+        return save;
+    }
 }
