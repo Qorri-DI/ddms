ed6d2e05715f9d03f27adc5e0cf18b66f43bbe6f
Anton Wijaya <antonhuang91@gmail.com>
2022-04-01 12:28:16 +0700

    Update ResponseDto.java

--- src/main/java/com/alpha/ddms/dto/ResponseDto.java
+++ src/main/java/com/alpha/ddms/dto/ResponseDto.java
@@ -1,19 +1,15 @@
 package com.alpha.ddms.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class ResponseDto<T> {
     private String status;
     private Integer code;
     private String message;
     private T data;
-
-    public ResponseDto(String status, Integer code, String message, T data) {
-        this.status = status;
-        this.code = code;
-        this.message = message;
-        this.data = data;
-    }
-
 }
