8ec7680114c47858cc9534f4b8dae1b94cbb63da
Anton Wijaya <antonhuang91@gmail.com>
2022-04-01 15:06:00 +0700

    merge dengan branch fahrur

--- src/main/java/com/alpha/ddms/DTO/DealerDTOlist.java
+++ src/main/java/com/alpha/ddms/DTO/DealerDTOlist.java
@@ -11,19 +11,4 @@ public class DealerDTOlist {
     List<DealerDTO> listDealer ;
     private int dataOfRecord;
 
-    public List<DealerDTO> getListDealer() {
-        return listDealer;
-    }
-
-    public void setListDealer(List<DealerDTO> listDealer) {
-        this.listDealer = listDealer;
-    }
-
-    public int getDataOfRecord() {
-        return dataOfRecord;
-    }
-
-    public void setDataOfRecord(int dataOfRecord) {
-        this.dataOfRecord = dataOfRecord;
-    }
 }
--- src/main/java/com/alpha/ddms/DTO/DealerDtoPost.java
+++ src/main/java/com/alpha/ddms/DTO/DealerDtoPost.java
@@ -11,35 +11,4 @@ public class DealerDtoPost {
     private String massage;
     private String status;
 
-    public int getCode() {
-        return code;
-    }
-
-    public void setCode(int code) {
-        this.code = code;
-    }
-
-    public List<DealerDTO> getData() {
-        return data;
-    }
-
-    public void setData(List<DealerDTO> data) {
-        this.data = data;
-    }
-
-    public String getMassage() {
-        return massage;
-    }
-
-    public void setMassage(String massage) {
-        this.massage = massage;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
 }
